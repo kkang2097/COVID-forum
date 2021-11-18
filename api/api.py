750c55ed4538b909298c06bbfc3c8cea65f12f22
Elliot Kang <kkang2097@gmail.com>
2021-11-17 21:54:58 -0800

    Docker isn't quite right

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import time
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,6 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
