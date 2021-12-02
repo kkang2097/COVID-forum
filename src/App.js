d299c66d76d8b46c82940b4bf94b1d62d210fa13
Elliot Kang <kkang2097@gmail.com>
2021-12-01 23:15:35 -0800

    elliot's mashup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import {React, Component} from 'react';
-import './App.css';
-import Header from './Header';
+import { React } from 'react';
+import {Route, Switch} from 'react-router-dom'
+import Navbar from './Navbar';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <a> abcd</a>  
+    <div>
+      <Navbar />
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/register" component={Register}></Route>
+        {/* <Route path="/feed" component={}></Route>
+        <Route path="/qa" component={}></Route> */}
+        <Route path="/" component={Home}></Route>
+      </Switch>
     </div>
   );
 }
