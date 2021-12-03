02c35a1a99b1c44922e25043f2fbafdfb1627a44
ciyuan53 <haoyuan.t@outlook.com>
2021-12-02 20:16:18 -0800

    add auth to home page (backend not routed yet)

--- src/Home.js
+++ src/Home.js
@@ -24,22 +24,43 @@ export default function Home() {
     };
 
     const slideRight = () => {
-        if (index + 1 < fakeData.length) { // change it to the fetched data
+        if (index + 1 < fakeData.length) {
             setIndex(index + 1);
         } else {
             setIndex(0)
         }
     };
 
+    // authenticate to check if a user is logged in
+    fetch('http://127.0.0.1:5000/auth', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(window.localStorage.getItem("Token"))
+    })
+        .then(response => {
+            return response.json()
+        })
+        .then((json) => {
+            if (json['success'] == true) {
+                return (
+                    <div className="card-carousel">
+                        <img src={left} onMouseDown={slideLeft} />
+                        <div className="card">
+                            <h1>{fakeData[index]["age"]}</h1>
+                            <h2>{fakeData[index]["gender"]}</h2>
+                            <h3>{fakeData[index]["location"]}</h3>
+                        </div>
+                        <img src={right} onMouseDown={slideRight} />
+                    </div>
+                )
+            } else {
+                return (
+                    <div>No one is logged in. (Username/Password didn't match)</div>
+                )
+            }
+        })
+
     return (
-        <div className="card-carousel">
-            <img src={left} onMouseDown={slideLeft} />
-            <div className="card">
-                <h1>{fakeData[index]["age"]}</h1>
-                <h2>{fakeData[index]["gender"]}</h2>
-                <h3>{fakeData[index]["location"]}</h3>
-            </div>
-            <img src={right} onMouseDown={slideRight} />
-        </div>
+        <div>auth() failed, 99% was because it did't run in api.py</div>
     )
 }
