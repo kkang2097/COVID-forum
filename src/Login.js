ed32c3429aa03609b9f927b4a6547e52a828b15e
chrisphum <76226574+chrisphum@users.noreply.github.com>
2021-12-02 23:06:57 -0800

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -8,30 +8,52 @@ export default function Login(props) {
 
     const attemptLogin = (event) => {
         event.preventDefault();
-        // Connect to the backend to check if the user is in our database
-        // let data = {};
-        // var data = { "Email": Email, "Pass": Pass };
-        // var x = fetch('http://127.0.0.1:5000/login', {
-        //     method: 'POST',
-        //     headers: { "Content-Type": "application/json" },
-        //     body: JSON.stringify(data)
-        // })
-        //     .then(response => {
-        //         return response.json()
-        //     })
-        //     .then((json) => {
-        //         var token = json['Token'];
-        //         var error = json['Error'];
-        //         if (error != false) {
-        //             this.error2 = error
-        //             this.forceUpdate()
-        //         }
-        //         else {
-        //             window.localStorage.setItem("Token", token);
-        //             // TODO
-        //             // Redirect to next window
-        //         }
-        //     });
+
+
+
+
+
+        // Christopher code:
+        var data = { "email": email, "password": password };
+        var x = fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then((json) => {
+                console.log(json)
+
+                var error = json["error"]
+                var token = json["token"]
+
+                if ( error == false )
+                {
+                    // The user logged in successfully 
+
+                    window.localStorage.setItem("Token", token);
+
+                    // Lets get some user information
+                    var userData = json["userData"]
+                    var name = userData["name"]
+                    var numPersons = userData["numPersons"]
+                    var state = userData["state"]
+                    // There is lots more userdata to use, like risk score, etc
+
+                }
+                else 
+                {
+                    console.log(error)
+                    // The user did not login succesfully
+                    // So error contains a string like "password is not correct" or "connection issues to MongoDB"
+                   // Display the error to the user
+                }
+
+                // Then redirect to next page
+
+            });
 
         history.push("/");
     }
