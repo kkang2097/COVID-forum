861067a29740c9831d31ccf9c20198907a6e0e06
Elliot Kang <kkang2097@gmail.com>
2021-12-02 18:57:49 -0800

    axios setup

--- src/actions/axios-help.js
+++ src/actions/axios-help.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const instance = axios.create({
   //In production, we do a different URL
-  baseURL: 'http://localhost:5090/'
+  baseURL: 'http://localhost:5000/'
 })
 
 export default instance;
