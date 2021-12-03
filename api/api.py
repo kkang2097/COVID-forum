8340ed34f16d06cc5795e122dd264059c8d54209
Elliot Kang <kkang2097@gmail.com>
2021-12-02 21:12:43 -0800

    api.py works again

--- api/api.py
+++ api/api.py
@@ -1,20 +1,138 @@
-import time
-from flask import Flask
+from sys import dont_write_bytecode
+from flask import Flask, render_template, request, redirect, url_for, request
+import requests
+from flask_cors import CORS
+
+#error here
+import authenticateUser
+import addUser
+import userClasses
+import chat
+
 #Can run 'pythom -m flask run' to test the API
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+CORS(app)
 
-
+#Basic API checking
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
-
-
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
-
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+#Author: Chris Humphrey
+@app.route("/auth", methods=['POST','GET'])
+def auth():
+    if request.method == "POST":
+        token = request.json
+        userData, token, error = authenticateUser.authenticateToken(token)
+
+        if error == False:
+            del userData.__dict__["_id"];
+            del userData.__dict__["password"];
+
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+    if request.method == "POST":
+        loginInfo = request.json
+        userData, token, error = authenticateUser.loginUser( loginInfo['email'], loginInfo['password'])
+
+        if error == False:
+            del userData.__dict__["_id"];
+            del userData.__dict__["password"];
+
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+
+@app.route("/newAcct", methods=['POST','GET'])
+def newAcct():
+    if request.method == "POST":
+
+        newAcctInfo = request.json
+        userData, token, error = addUser.addUser(newAcctInfo["email"], newAcctInfo["password"], \
+            newAcctInfo["name"], newAcctInfo["state"], newAcctInfo["emailUpdates"])
+
+        if error == False:
+            del userData.__dict__["_id"];
+            del userData.__dict__["password"];
+
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+@app.route("/addPerson", methods=['POST','GET'])
+def addPerson():
+    if request.method == "POST":
+        newPersonInfo = request.json
+        token = newPersonInfo["token"]
+        userData, token, error = authenticateUser.authenticateToken(token)
+
+        if error == False:
+            userData, error = addUser.addPerson(userData, newPersonInfo['name'], newPersonInfo['age'], \
+                    newPersonInfo['sex'], newPersonInfo['state'], newPersonInfo['vaccine'], newPersonInfo['race'], \
+                    newPersonInfo['smoker'], newPersonInfo['heightFeet'], newPersonInfo['heightInches'], newPersonInfo['heartLung'], newPersonInfo['mask'] )
+            del userData.__dict__["_id"];
+            del userData.__dict__["password"];
+
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+
+@app.route("/removePerson", methods=['POST','GET'])
+def removePerson():
+    if request.method == "POST":
+        data = request.json
+        token = data["token"]
+        personToRemove = data["personToRemove"]
+        userData, token, error = authenticateUser.authenticateToken(token)
+
+        if error == False:
+
+            userData, error = addUser.removePerson(userData, personToRemove)
+
+        del userData.__dict__["_id"];
+        del userData.__dict__["password"];
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+@app.route("/addChat", methods=['POST','GET'])
+def addChat():
+    if request.method == "POST":
+        data = request.json
+        token = data["token"]
+        message = data["message"]
+        userData, token, error = authenticateUser.authenticateToken(token)
+
+        if error == False:
+            name = userData["name"]
+            state = userData["state"]
+            error = chat.addMessage(name, state, message)
+
+        del userData.__dict__["_id"];
+        del userData.__dict__["password"];
+        response = {"userData": userData.__dict__, "token": token, "error": error}
+        return response
+
+@app.route("/getChat", methods=['POST','GET'])
+def getChat():
+
+     if request.method == "POST":
+        data = request.json
+        token = data["token"]
+        userData, token, error = authenticateUser.authenticateToken(token)
+
+        if error == False:
+            messages, error = chat.getMessages()
+
+        del userData.__dict__["_id"];
+        del userData.__dict__["password"];
+        response = {"userData": userData.__dict__, "messages": messages, "token": token, "error": error}
+        return response
---
+++ api/chat.py
@@ -0,0 +1,29 @@
+import datetime
+import re
+import bson
+import userClasses
+import passwordHash
+import jwt
+import requests
+import pymongo
+
+cluster = pymongo.MongoClient("mongodb+srv://chris:12345@cluster0.6zsms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["test"]
+collection = db["statuses"]
+
+def addMessage(name, state, message):
+    today = datetime.today()
+    date = today.strftime("%d/%m/%Y")
+    x = collection.insert_one({"name": name}, {"state": state}, {"date": date}, {"message": message})
+    if x == None:
+        return "Error Adding Status"
+    else:
+        return False
+
+def getMessages():
+    x = collection.find()
+    if x == None:
+        return None, "Error Getting Status"
+    else:
+        return x, None
+
