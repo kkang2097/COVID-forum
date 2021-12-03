03973ebe2cdab7fde26a5f48e66b95d985f63da8
Elliot Kang <kkang2097@gmail.com>
2021-12-02 17:38:22 -0800

    example Axios calls/Unit-test boilerplate

--- src/axios-help/index.js
+++ src/axios-help/index.js
@@ -2,3 +2,18 @@ const axios = require('./base-instance.js');
 
 
 //Functions here
+module.exports = {
+
+  async function getUserToken (user) {
+    const token = await axios.get('/api/getToken', user);
+    return token;
+  }
+
+  async function postUserToken (user) {
+    const token = await axios.post('/api/postToken', user);
+    return token;
+  }
+
+
+  
+}
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+//Kevin Tran is the master Unit Tester!
+//His code is here:
