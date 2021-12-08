56218b48cc017514eba48b67ae0611ddc63ee3a9
ciyuan53 <haoyuan.t@outlook.com>
2021-12-07 18:08:18 -0800

    navbar slight change

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,43 +5,44 @@ import { useDataLayerValue } from "./DataLayer";
 
 export default function Navbar() {
 
-    const [{  isLoggedIn, name }, dispatch] = useDataLayerValue();
+    const [{ isLoggedIn, name }, dispatch] = useDataLayerValue();
 
     const attemptLogout = (event) => {
         window.localStorage.removeItem('Token')
         var response = {
             type: "UPDATE_USER",
-            isLoggedIn: false}
+            isLoggedIn: false
+        }
         dispatch(response);
         window.location.href = "/";
-        }
+    }
 
     // IMPORTANT PLEASE READ
     // We need two Navigation bars, depending on if the variable "isLoggedIn" is true or false
     // If the variable isLoggedIn is true show the logged in Nav Bar
     // Else show the logged out Nav Bar
     // The variable "name" will be the user's first name
-    
-    if ( isLoggedIn == true) {
+
+    if (isLoggedIn == true) {
         return (
-            <div className="navbar">
+            <nav className="navbar">
                 <Link to="/">HOME</Link>
                 <Link to="/qa">Q & A</Link>
                 <Link to="/feed">Feed</Link>
                 <Link to="/addperson">AddPerson</Link>
                 <Link to="/riskscores">See Risk Score</Link>
-                <Link to='/'  onClick={attemptLogout}>LOGOUT</Link>
-            </div>
+                <Link to='/' onClick={attemptLogout}>LOGOUT</Link>
+            </nav>
         )
     }
     else {
-        return(
-            <div className="navbar">
+        return (
+            <nav className="nav-bar">
                 <Link to="/">HOME</Link>
                 <Link to="/qa">Q & A</Link>
                 <Link to="/feed">Feed</Link>
                 <Link to="/login">LOGIN</Link>
-            </div>
+            </nav>
         )
     }
 }
