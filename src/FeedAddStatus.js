b6d52c5c3f60754ca2f8209bfefed6ae6760c098
Elliot Kang <kkang2097@gmail.com>
2021-12-11 00:14:28 -0800

    Feed functions boilerplate

---
+++ src/FeedAddStatus.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function FeedAddStatus() {
+
+  useEffect(
+    () => {
+      //Code Here
+
+    }
+  ,[]);
+
+  return(
+
+
+  );
+}
+
+
+
+
+export default FeedAddStatus;
