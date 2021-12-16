e9275fc8be6061d9a2cce77a9aef3e0fc2bd6072
chrisphum <76226574+chrisphum@users.noreply.github.com>
2021-12-16 12:54:45 -0800

    Add files via upload

--- api/userClasses.py
+++ api/userClasses.py
@@ -1,5 +1,7 @@
 from os import fdopen
 
+from pymongo.common import _CaseInsensitiveDictionary
+
 
 class User:
     def __init__(self, _id=None, email=None, password=None, name=None, state=None, emailUpdates=None, numPersons=None, person1=None, person2=None, person3=None):
@@ -16,6 +18,7 @@ class User:
 
 class Person:
     def __init__ (self, name=None, age=None, sex=None, state=None, vaccine=None, race=None, smoker=None, heightFeet=None, heightInches=None, heartLung=None, mask=None):
+
         self.name = name #string
         self.age = age #int
         self.sex = sex #int: 0 for female, 1 for male
@@ -27,6 +30,46 @@ class Person:
         self.heightInches = heightInches #int
         self.heartLung = heartLung #int: 0 for no disease, 1 for yes disease
         self.mask = mask #int: 1 for never wears mask, 5 for always wears mask
-        self.riskScore = "100"
         self.dateLastUpdateRiskScore = "TBD"
+        
+        age = int(age)
+        print(age)
+        risk = 50
+        if age > 75:
+            risk = risk * 8
+        elif age > 75:
+            risk = risk * 3
+        elif age > 45:
+            risk = risk * 1
+        elif age > 20:
+            risk = risk * 0.1
+        else:
+            risk = 0
 
+        if vaccine:
+            risk = risk * 0.3
+
+        self.riskScore = risk / 1.3
+
+
+    
+# def retrieve_State_Data(state):
+#     from pymongo import MongoClient
+#     cluster = MongoClient("mongodb+srv://chris:12345@cluster0.6zsms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#     db = cluster["test"]
+#     collection = db["state"]
+#     return collection.find_one({"state": state}) 
+
+# def risk_Algorithm(state="CA", sex=0, age=50, vaccine=1):
+#     print(age)
+
+#     # caseDensity =  retrieve_State_Data(state)['caseDensity']
+#     caseDensity = 50.00
+#     sex = 0
+#     # Men make up 1.4 times as many deaths
+#     # https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/
+#     risk = ( caseDensity / 1.2 + (caseDensity * 0.4 * sex) ) / 1.2 #with men being 1 and women 0
+
+
+
+#     return risk
