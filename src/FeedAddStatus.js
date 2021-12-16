16c7776baf462344c917172eb0514b1d25bae0e6
LukieLucky <78717315+LukieLucky@users.noreply.github.com>
2021-12-16 14:13:39 +0800

    edited for styling

--- src/FeedAddStatus.js
+++ src/FeedAddStatus.js
@@ -22,27 +22,26 @@ function FeedAddStatus() {
       "message": message,
       "city": city
     };
-    var json = Call("addChat", data)
-    .then(response => {
+    Call("addChat", data)
+    .then((response => {
         if (response.error == false) {
             console.log("ADDED STATUS UPDATE");
         }
         else {
             console.log(response.error);
         }
-    });
+    }));
   }
 
   return(
     <div className = "add-feed-status">
-      <input type = "text" id = "message" value = {message}
+      <textarea type = "text" id = "message" value = {message}
       placeholder = "If you have COVID-19, share your experience!"
       onChange = {e => {setMessage(e.target.value)} }/>
-
       <input type="text" id="city" value = {city}
       placeholder="City (Name)"
       onChange = {e => (setCity(e.target.value))}/>
-      <button onClick = {e => {
+      <button class='search' onClick = {e => {
         handleAddStatus()
       } }>Add Status</button>
     </div>
